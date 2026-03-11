cfada831cf262bbdee76e7eae993da0ef336e354
Sala 7 <sala7@FI-LabSistemas.ucatolica.loc>
2026-03-11 07:52:46 -0500

    Se agrega enum de posiciones de futbol

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"mensaje": "API de jugadores funcionando"}
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
+
+class PosicionFutbol(str, Enum):
+    PORTERO = "PORTERO"
+    DEFENSA = "DEFENSA"
+    MEDIOCAMPISTA = "MEDIOCAMPISTA"
+    DELANTERO = "DELANTERO"
+    EXTREMO = "EXTREMO"
+
+@app.get("/")
+def home():
+    return {"mensaje": "API de jugadores funcionando"}
